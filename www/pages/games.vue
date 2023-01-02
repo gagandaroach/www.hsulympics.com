893022b8f0691530236cce5a4bafc2a5e62aef6b
gagany <gagandaroach@gmail.com>
2023-01-02 19:36:42 +0000

    stylize games

--- www/pages/games.vue
+++ www/pages/games.vue
@@ -2,12 +2,17 @@
   <section>
     <div class="">
       <div v-if="!pending" v-for="(game, index) in sheets['Games']" :key="index">
-        <div v-if="game.show === 'TRUE'" class="container flex flex-col bg-blue-100 rounded-lg m-3 mx-auto p-3">
-          <h1 class="text-xl mb-2">{{game.name}}</h1>
-          <p>Objective: {{ game.obj }}</p>
-          <p>_id: {{ game.id }}</p>
-          <p>_show: {{ game.show }}</p>
-          <p>_dump: {{ game }}</p>
+        <div v-if="game.show === 'TRUE'" class="container flex flex-col text-white bg-black border-2 border-solid border-red-900 rounded-lg m-3 mx-auto p-3">
+          <h1 class="text-2xl text-yellow-400 mb-2">{{game.name}}</h1>
+          <p class="text-base text-red-400">Objective</p>
+          <p>{{ game.obj }}</p>
+          <p class="text-base text-red-400">Rules</p>
+          <p>{{ game.rules }}</p>
+          <p class="text-base text-red-400">Format</p>
+          <p>{{ game.format }}</p>
+          <p class="text-base text-red-400">Scoring</p>
+          <p>{{ game.scoring }}</p>
+          <!-- <p>_dump: {{ game }}</p> -->
         </div>
       </div>
       <div v-else>
