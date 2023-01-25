ab307afd2665b9e220d35fdfe127570f21f86538
gagany <gagandaroach@gmail.com>
2023-01-25 06:42:03 +0000

    team score ties

--- www/pages/scoreboard.vue
+++ www/pages/scoreboard.vue
@@ -8,7 +8,8 @@
       <div class="flex mx-auto">
         <div v-if="!hsuStore.loaded" class="text-white text-center">Loading...</div>
         <div v-else class="text-white mx-auto">
-          <table :class="tableClass">
+          <div class="flex flex-col mr-1">
+            <table :class="tableClass">
             <thead>
               <tr>
                 <th :class="`${tableHeaderBaseClass} border-b-2`">
@@ -17,7 +18,7 @@
                   </div>
               </th>
                 <th
-                  v-for="(score, index) in activeGameScores(hsuStore.scores)"
+                  v-for="(score, index) in hsuStore.activeGameScores"
                   :key="index"
                   :class="`${tableHeaderBaseClass} border-b-2 text-yellow-400`"
                 >
@@ -38,7 +39,7 @@
               <tr v-for="(team, index) in hsuStore.teams" :key="index">
                 <td :class="`${tableRowBaseClass} border-r-2 border-hsu-red`">{{ team.name }}</td>
                 <td
-                  v-for="(score, index2) in activeGameScores(hsuStore.scores)"
+                  v-for="(score, index2) in hsuStore.activeGameScores"
                   :key="index2"
                   :class="`${tableRowBaseClass} text-6xl border-b-2`"
                 >
@@ -50,9 +51,10 @@
                       <!-- {{ score.scores[team.id - 1] }} -->
                       <img :src="placement_to_trophy_svg(score.scores[team.id - 1])" alt="" class="bg-black">
                     </div>
-                    <div class="flex mx-auto text-right">
+                    <div class="flex mx-auto p-4">
                       <p>
-                        {{ placement_to_score(score.scores[team.id - 1]) }}
+                        <!-- {{ placement_to_score(score.scores[team.id - 1]) }} -->
+                        {{ hsuStore.teamScoresForGame(score.id)[team.id] }}
                       </p>
                     </div>
                   </div>
@@ -63,20 +65,30 @@
               </tr>
             </tbody>
           </table>
-
-          <div class="text-sm md:text-base text-white text-end p-1">
+          <div class="text-sm md:text-base text-slate-500 text-center p-1">
                 Last Updated: {{ hsuStore.lastUpdated }}
           </div>
+        </div>
+
 
           <div class="mt-16">
             <div class="text-xl">Team Placement Stats:</div>
-            <div>will use these computed props to dynamically color table</div>
+            <!-- <div>will use these computed props to dynamically color table</div>
             <div>
               team placement : {{ winningTeamIndexes(hsuStore.scores) }}
             </div>
             <div>team 1 in first? : {{ isInFirst(hsuStore.scores, 0) }}</div>
             <div>team 2 in second? : {{ isInSecond(hsuStore.scores, 1) }}</div>
-            <div>team 4 in second? : {{ isInSecond(hsuStore.scores, 3) }}</div>
+            <div>team 4 in second? : {{ isInSecond(hsuStore.scores, 3) }}</div> -->
+            <!-- <div>teamScoresForGame(0): {{ hsuStore.teamScoresForGame(0) }}</div> -->
+            <div>teamPositionsForGame(1): {{ hsuStore.teamPositionsForGame(1) }}</div>
+            <div>teamPositionsForGame(2): {{ hsuStore.teamPositionsForGame(2) }}</div>
+            <div>teamPositionsForGame(7): {{ hsuStore.teamPositionsForGame(7) }}</div>
+            <div>teamScoresForGame(1): {{ hsuStore.teamScoresForGame(1) }}</div>
+            <div>teamScoresForGame(7): {{ hsuStore.teamScoresForGame(7) }}</div>
+            <!-- <div>activeGames: {{ hsuStore.activeGames }}</div> -->
+            <!-- <div>teams: {{ hsuStore.teams }}</div> -->
+            <!-- <div>activeGameScores: {{ hsuStore.activeGameScores }}</div> -->
           </div>
         </div>
       </div>
@@ -94,10 +106,47 @@ import place3 from "assets/svgs/place3.svg";
 import place4 from "assets/svgs/place4.svg";
 import place5 from "assets/svgs/place5.svg";
 
+const tableClass ="table-auto border-collapse mt-8 bg-black mx-auto";
+const tableHeaderBaseClass = `md:text-2xl text-base border-hsu-red my-2`;
+const tableRowBaseClass = `text-4xl text-center h-12 w-auto border-slate-500 p-2`;
+
 const hsuStore = useHsuDataStore()
 if (!hsuStore.loaded) {
   hsuStore.refreshSheets()
 };
+
+useHead({
+  title: "Scoreboard",
+});
+
+const hideScoreboard = false;
+const rotateTable = useState("rotateTable", () => false);
+const { pause, resume, isActive } = useIntervalFn(() => {
+  if (!hideScoreboard) {
+    hsuStore.refreshSheets()
+  }
+}, 50000);
+
+function computeTotalScore(game_scores_sheet, team_index) {
+  let score = 0;
+  for (const game in game_scores_sheet) {
+    if (Object.hasOwnProperty.call(game_scores_sheet, game)) {
+      const game_score = game_scores_sheet[game];
+      if (game_score.show === "TRUE") {
+        const curr_score = game_score.scores[team_index];
+        if (curr_score !== '--')
+        {
+          score += parseInt(placement_to_score(curr_score));
+        }
+      }
+    }
+  }
+  if (score == 0)
+  {
+    return '--'
+  }
+  return score;
+}
 
 function placement_to_trophy_svg(placement) {
   switch (placement) {
@@ -139,112 +188,5 @@ function placement_to_score(placement) {
     default:
       return "--"
   }
-}
-
-useHead({
-  title: "Scoreboard",
-});
-
-const hideScoreboard = false;
-
-const rotateTable = useState("rotateTable", () => false);
-
-const { pause, resume, isActive } = useIntervalFn(() => {
-  if (!hideScoreboard) {
-    hsuStore.refreshSheets()
-  }
-}, 5000);
-
-const tableClass ="table-auto border-collapse mt-8 bg-black";
-const tableHeaderBaseClass = `md:text-2xl text-base border-hsu-red my-2`;
-const tableRowBaseClass = `text-4xl text-center h-12 w-auto border-slate-500 p-2`;
-
-function computeTotalScore(game_scores_sheet, team_index) {
-  let score = 0;
-  for (const game in game_scores_sheet) {
-    if (Object.hasOwnProperty.call(game_scores_sheet, game)) {
-      const game_score = game_scores_sheet[game];
-      if (game_score.show === "TRUE") {
-        const curr_score = game_score.scores[team_index];
-        if (curr_score !== '--')
-        {
-          score += parseInt(placement_to_score(curr_score));
-        }
-      }
-    }
-  }
-  if (score == 0)
-  {
-    return '--'
-  }
-  return score;
-}
-
-function activeGameScores(game_scores_sheet) {
-  let active_scores = {};
-  for (const game in game_scores_sheet) {
-    if (Object.hasOwnProperty.call(game_scores_sheet, game)) {
-      const game_score = game_scores_sheet[game];
-      if (game_score.show === "TRUE") {
-        active_scores[game] = game_score;
-      }
-    }
-  }
-  return active_scores;
-}
-
-function isInSecond(game_scores_sheet, team_index) {
-  return winningTeamIndexes(game_scores_sheet)["second"].includes(team_index);
-}
-
-function isInFirst(game_scores_sheet, team_index) {
-  return winningTeamIndexes(game_scores_sheet)["first"].includes(team_index);
-}
-
-function winningTeamIndexes(game_scores_sheet) {
-  let num_teams = null;
-
-  // Get Num Teams from the first game score's length
-  for (const game in game_scores_sheet) {
-    if (Object.hasOwnProperty.call(game_scores_sheet, game)) {
-      const game_score = game_scores_sheet[game];
-      if (num_teams === null) {
-        num_teams = game_score.scores.length;
-        break;
-      }
-    }
-  }
-
-  // Compute all Scores
-  let scores = [];
-  let max = 0;
-  let secondMax = 0;
-  for (let index = 0; index < num_teams; index++) {
-    const score = computeTotalScore(game_scores_sheet, index);
-    scores.push(score);
-    if (score > max) {
-      max = score;
-    }
-    if (score < max && score > secondMax) {
-      secondMax = score;
-    }
-  }
-
-  // Get Max Score IDx
-  let max_idx = [];
-  let second_idx = [];
-  for (let index = 0; index < num_teams; index++) {
-    if (scores[index] === max) {
-      max_idx.push(index);
-    }
-    if (scores[index] === secondMax) {
-      second_idx.push(index);
-    }
-  }
-
-  return {
-    first: max_idx,
-    second: second_idx,
-  };
 }
 </script>
--- www/stores/hsuData.js
+++ www/stores/hsuData.js
@@ -1,5 +1,13 @@
 import { defineStore } from 'pinia'
 
+const placement_to_score_map = {
+    1: 20,
+    2: 16,
+    3: 13,
+    4: 10,
+    5: 8
+}
+
 export const useHsuDataStore = defineStore('hsuStore', {
     state: () => ({
         sheets: {},
@@ -14,9 +22,30 @@ export const useHsuDataStore = defineStore('hsuStore', {
         error: null
     }),
     getters: {
-        tedams(state) {
-            return state.sheets['Teams']
+        activeGames(state) {
+            let active = {};
+            for (const game in state.games) {
+                if (Object.hasOwnProperty.call(state.games, game)) {
+                    const game_score = state.games[game];
+                    if (game_score.show === "TRUE") {
+                        active[game] = game_score;
+                    }
+                }
+            }
+            return active;
         },
+        activeGameScores(state) {
+            let active = {};
+            for (const gs in state.scores) {
+                if (Object.hasOwnProperty.call(state.scores, gs)) {
+                    const score = state.scores[gs];
+                    if (score.show === "TRUE") {
+                        active[gs] = score;
+                    }
+                }
+            }
+            return active;
+        }
     },
     actions: {
         async refreshSheets() {
@@ -48,6 +77,81 @@ export const useHsuDataStore = defineStore('hsuStore', {
                 }
             }
             return team_players;
+        },
+        teamPositionsForGame(game_idx) {
+            // { "id": "1", "scores": [ "1", "2", "4", "5", "2" ], "show": "TRUE" }
+            // { "id": "7", "scores": [ "--", "--", "--", "--", "--" ], "show": "TRUE" }
+            const game_score = this.scores[game_idx];
+            const game_placements = game_score.scores;
+
+            const placements = {}
+            for (let index = 1; index <= game_placements.length; index++) {
+                placements[index.toString()] = []
+                if (game_placements[index-1] == '--')
+                {
+                    return []
+                }
+            }
+
+            for (let index = 1; index <= game_placements.length; index++) {
+                const score = game_placements[index-1]
+                // console.log(score)
+                placements[score].push(index)
+            }
+
+            return placements;
+        },
+        teamScoresForGame(game_idx) {
+            // { "1": [ 1 ], "2": [ 2, 5 ], "3": [], "4": [ 3 ], "5": [ 4 ] }
+            // { "1": [ 4 ], "2": [ 3 ], "3": [ 5 ], "4": [ 1 ], "5": [ 2 ] }
+            const team_placements = this.teamPositionsForGame(game_idx);
+
+            // init scores
+            // { "1": 0, "2": 0, "3": 0, "4": 0, "5": 0 }
+            const team_scores = {}
+            const game_score = this.scores[game_idx];
+            for (let idx = 0; idx < game_score.scores.length; idx++) {
+                team_scores[idx + 1] = 0;
+            }
+            
+            // teamPositions is undefined, have -- in score :( 
+            // so return 0 for all teams
+            if (team_placements.length == 0)
+            {
+                console.log(`game ${game_idx} has -- score, returning 0 for all teams.`)
+                return team_scores;
+            }
+
+            // for place in placements
+            for (let place = 1; place <= Object.keys(team_placements).length; place++) {
+
+                // Only one team placed at position, set their score
+                if (team_placements[place].length == 1)
+                {
+                    const team_id = team_placements[place];
+                    team_scores[team_id] = placement_to_score_map[place];
+                }
+
+                // Multiple teams tied for position
+                else if (team_placements[place].length >= 1)
+                {
+                    const num_tied_teams = team_placements[place].length;
+
+                    // Compute the tied
+                    let total_tie_score = 0
+                    for (let idx2 = 0; idx2 < num_tied_teams; idx2++) {
+                        total_tie_score = total_tie_score + placement_to_score_map[place + idx2];
+                    }
+                    const tie_score = total_tie_score / num_tied_teams;
+
+                    for (let team_tied_idx = 0; team_tied_idx <= num_tied_teams; team_tied_idx++)
+                    {
+                        const team_id = team_placements[place][team_tied_idx];
+                        team_scores[team_id] = tie_score;
+                    }
+                }
+            }
+            return team_scores
         }
     }
 })
